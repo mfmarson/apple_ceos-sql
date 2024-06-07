2d7c6a5a2c9165c804879920a61acf06c27bf525
Megan <megan.marson22@gmail.com>
2024-06-06 20:50:22 -0400

    Inital commit for SQL exercises

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE apple_ceos (
+    id SERIAL PRIMARY KEY,
+    Name TEXT NOT NULL
+);
+
+CREATE TABLE apple_ceos(
+    id  SERIAL PRIMARY KEY,
+    Year INTEGER
+);
---
+++ seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO apple_ceos (name)(year) VALUES ('Michael Spindler')(1993),('Gil Amelio')(1996),('Mike Markkula')(1977), ('Tim Cook')(2011),('Mike Scott')(1978), ('John Sculley')(1983), ('Steve Jobs')(1976), ('Steve Jobs')(1997);
